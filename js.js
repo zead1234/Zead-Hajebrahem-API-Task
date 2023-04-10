e267db2175213618d320b846ba9de0ed2e9be1d6
zead112 <zeadhajebrahem123@gmail.com>
2023-04-10 21:53:47 +0400

    add event to images

--- js.js
+++ js.js
@@ -1,20 +1,22 @@
 const btn = document.querySelector(".input-area button");
 const product = document.querySelector(".product-area");
 
-function appendNewItemIntoproduct(value,image1,dsicrip){
-  
+function appendNewItemIntoproduct(value, images, dsicrip) {
   const p = document.createElement("p");
-  p.innerText =value;
-  p.classList ="title";
+  p.innerText = value;
+  p.classList = "title";
   const image = document.createElement("img");
-  image.src=image1;
-  image.className="im";
-  image.alt="there is some thing lost"
+  image.src = images[0];
+  image.className = "im";
+  image.alt = "there is some thing lost";
+  image.addEventListener('click', () => {
+    const imageUrls = images.map(img => `<img src="${img}"/>`).join('');
+    window.open().document.write(imageUrls);
+  });
   const disc = document.createElement("p");
-  disc.innerText=dsicrip;
-  disc.className="disc";
+  disc.innerText = dsicrip;
+  disc.className = "disc";
   const div = document.createElement("div");
- 
   const button = document.createElement("button");
   button.classList.add("order");
   button.innerText = "Order";
@@ -22,37 +24,31 @@ function appendNewItemIntoproduct(value,image1,dsicrip){
   div.appendChild(image);
   div.appendChild(disc);
   div.appendChild(button);
-
   product.appendChild(div);
 }
-let currentPage=1;
-async function firstItems(){
-let data=[];
-await fetch(`https://dummyjson.com/products?page=${currentPage}&limit=10`)
-.then(res => res.json())
-.then(res => data=res.products);
-
-data.forEach((item) => {
-    appendNewItemIntoproduct(item.title,item.images[1],item.description);
+let currentPage = 1;
+async function firstItems() {
+  let data = [];
+  await fetch(`https://dummyjson.com/products?page=${currentPage}&limit=10`)
+    .then(res => res.json())
+    .then(res => data = res.products);
+  data.forEach((item) => {
+    appendNewItemIntoproduct(item.title, item.images, item.description);
   });
 }
 firstItems();
 
-
-
-async function nextItems(){
- let newData=[];
+async function nextItems() {
+  let newData = [];
   currentPage++;
   await fetch(`https://dummyjson.com/products?page=${currentPage}&limit=10`)
-.then(res => res.json())
-.then(res => newData=res.products);
+    .then(res => res.json())
+    .then(res => newData = res.products);
   newData.forEach((item) => {
-    if(currentPage>=5){currentPage=1}
-    appendNewItemIntoproduct(item.title,item.images[currentPage],item.description);
+    if (currentPage >= 5) { currentPage = 1 }
+    appendNewItemIntoproduct(item.title, item.images, item.description);
   });
 }
-
-
 async function prevItems() {
   if (currentPage > 1) {
     currentPage--;
@@ -61,7 +57,7 @@ async function prevItems() {
       .then(res => res.products);
     product.innerHTML = '';
     data.forEach((item) => {
-      appendNewItemIntoproduct(item.title,item.images[1],item.description);
+      appendNewItemIntoproduct(item.title, item.images, item.description);
     });
   }
 }
